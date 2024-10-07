5056e9041c03bdb8bc44afb5780f3dd38a8ab196
Hannu Parviainen <hannu@iac.es>
2024-10-07 16:20:37 +0100

    Added a set of exercise examples.

---
+++ examples/ex03_matrix_multiplication/e1v1.f90
@@ -0,0 +1,39 @@
+program e1v1
+  implicit none
+  integer :: ix, iy, k
+  integer :: sax, say, sbx, sby
+  real :: A(3,4), B(4,3)
+  real, allocatable :: C(:,:)
+
+  say = size(A, 1)
+  sax = size(A, 2)
+  sby = size(B, 1)
+  sbx = size(B, 2)
+  
+  A = reshape([3, 2, 4, 1,  &
+       &       2, 4, 2, 2,  &
+       &       1, 2, 3, 7], &
+       & [say, sax], order=[2, 1])
+
+  B = reshape([3, 2, 4,  &
+       &       2, 4, 2,  &
+       &       1, 2, 3,  &
+       &       0, 2, 1], &
+       & [sby, sbx], order=[2, 1])
+
+  allocate(C(say, sbx))
+  
+  do iy = 1, say
+     do ix = 1, sbx
+        do k = 1, sax
+           C(iy, ix) = C(iy, ix) + A(iy, k) *  B(k, ix)
+        end do
+     end do
+  end do
+
+  do iy = 1, size(C, 1)
+     print '(*(F6.1,2X))', C(iy, :)
+  end do
+  
+end program e1v1
+  
