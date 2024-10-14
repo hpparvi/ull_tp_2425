5a7f3690b2fde46b69436542e50b7e3619c7932e
urmagt <alu0101348438@ull.edu.es>
2024-10-14 15:29:14 +0100

    Edited particle.f90 file

--- exercises/gu/ex1/particle.f90
+++ exercises/gu/ex1/particle.f90
@@ -2,8 +2,8 @@ module particle
   use geometry
   implicit none
   type particle
-     type(point3d) :: p !osition
-     type(vector3d) :: v !elocity
-     real :: m !ass
+     type(point3d) :: position
+     type(vector3d) :: velocity
+     real :: mass
   end type particle
 end module particle
