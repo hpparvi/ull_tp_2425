791f272210a0c816a4e7398e186f2818623fb1b0
Eva Sola <evasolavi@gmail.es>
2024-10-14 16:00:33 +0100

    Added particle module
    Added the particle module, which contains
    only the particle type, with a dependency
    on the geometry module.
    
    Untested yet.

---
+++ exercises/se/ex1/particle.f90
@@ -0,0 +1,12 @@
+MODULE particle
+  USE geometry
+  IMPLICIT NONE
+
+  TYPE particle
+     TYPE(point3d)  :: p ! Position
+     TYPE(vector3d) :: v ! Velocity
+     REAL           :: m ! Mass
+  END TYPE particle
+
+
+END MODULE particle
