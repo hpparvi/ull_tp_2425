3ba90190608c1bfb21f2677afbac3711ca3c392d
Cathaysa <cathaysaperdomo@outlook.es>
2024-10-15 20:19:11 +0100

    Adding modules for exercise 1 and Makefile

---
+++ exercises/pc/ex1/particle.f90
@@ -0,0 +1,10 @@
+module particle
+  use geometry
+  implicit none
+  type :: particle3d
+     type(point3d) :: p !particle's position
+     type(vector3d) :: v !particle's velocity
+     type(vector3d) :: a !particle's acceleration
+     real :: m !particles's mass
+  end type particle3d
+end module particle
