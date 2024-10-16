389ec58b3865b776b79ae862ff2ea1e1e5aa35e5
Xoan Vilas <xoanvilas99@gmail.com>
2024-10-16 15:31:01 +0100

    Some comments in ex01

--- exercises/vx/ex1/particle.f90
+++ exercises/vx/ex1/particle.f90
@@ -1,12 +1,12 @@
-module particl
+module particle
   use geometry
   implicit none
   
-  type ::  particle
+  type ::  particle3d
      type(point3d) :: p
      type(vector3d) :: v  !vector3d instead of 2d
      type(vector3d) :: a  !needed for next part
      real :: m
-  end type particle
+  end type particle3d
 
-end module particl
+end module particle
