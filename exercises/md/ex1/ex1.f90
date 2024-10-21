065063b7610808a6efd14a7b931b61fd1e7bb4db
DavidMirabal <alu0101326825@ull.edu.es>
2024-10-21 16:19:26 +0100

    More simple code

--- exercises/md/ex1/ex1.f90
+++ exercises/md/ex1/ex1.f90
@@ -4,14 +4,11 @@ program n_body
 
     implicit none
 
-    integer :: i, j
-    integer :: n
+    integer :: i, n
     real :: dt, t_end, t, dt_out, t_out
-    real :: r2, r3
     character(len=100) :: sim_name, file_out
 
     type(particle3d), allocatable :: particles(:)
-    type(vector3d) :: rji
 
     print*, "Enter the simulation name:"
     read*, sim_name
@@ -38,36 +35,19 @@ program n_body
     end do
 
     call reset_a(particles)
-
-    do i = 1, n
-        do j = i+1, n 
-            rji = particles(j)%p - particles(i)%p
-            r2 = normsquare(rji)
-            r3 = r2 * sqrt(r2)
-            particles(i)%a = particles(i)%a + particles(j)%m * rji / r3
-            particles(j)%a = particles(j)%a - particles(i)%m * rji / r3
-        end do
-    end do
+    call update_a(particles)
 
     t_out = 0.
     open(unit=1, file=file_out, action='write')
         write(1, "(A6, 1X, A12, 1X, A12, 1X, A12, 1X, A12)") "#Index", "X", "Y", "Z", "Mass"
         do while (t < t_end)
+            call update_vel(particles, dt)
             call update_pos(particles, dt)
-            call update_vel(particles, dt)
             call reset_a(particles)
-
-            do i = 1, n
-                do j = i+1, n 
-                    rji = particles(j)%p - particles(i)%p
-                    r2 = normsquare(rji)
-                    r3 = r2 * sqrt(r2)
-                    particles(i)%a = particles(i)%a + particles(j)%m * rji / r3
-                    particles(j)%a = particles(j)%a - particles(i)%m * rji / r3
-                end do
-            end do
-
+            call update_a(particles)
             call update_vel(particles, dt)
+
+
             t_out = t_out + dt
             if  (t_out >= dt_out) then
                 print*, "Positions at time ", t
@@ -80,6 +60,7 @@ program n_body
             end if
 
             t = t + dt
+            
         end do
     close(1)
 end program n_body
