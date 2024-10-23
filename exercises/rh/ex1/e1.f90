10cd826fba6cc529032a130dc36d9711b4c199fc
helena <alu0101331331@ull.edu.es>
2024-10-23 15:13:22 +0100

    exercise 1

--- exercises/rh/ex1/e1.f90
+++ exercises/rh/ex1/e1.f90
@@ -4,22 +4,83 @@ program e1
   use particle
   implicit none
 
-INTEGER :: i
-INTEGER :: n ! number of particles
-type(particle3d), dimension(:), allocatable :: particles ! particle type array (contains the particles info)
-real, dimension(:), allocatable :: m
+  INTEGER :: i, j ! loop indexes
+  INTEGER :: rc 
+  INTEGER :: n = 0 ! number of particles
+  REAL :: dt, t_end, t, dt_out, t_out ! time variables
+  type(particle3d), dimension(:), allocatable :: particles ! particle type array (contains the particles info)
+  type(vector3d), dimension(:), allocatable :: a ! aceleration array (for each particle) 
+  type(vector3d) :: rji
+  REAL :: r
+  CHARACTER(len=*), PARAMETER :: filename = 'initial_conditions.dat', outname = 'result.dat'
 
-READ *, n ! number of particles
-ALLOCATE(particles(n)) 
-ALLOCATE(m(n)) 
 
-! probably this part is easier using a file with the particle info, but oky
-DO i = 1, n
-  PRINT *, 'Insert the info of the particle: '
-  READ *, particles(i)%m, particles(i)%p, particles(i)%v !intro values for each particle of mass, position and velocity  
-END DO
+  OPEN (file = filename, action = 'read', status = 'old', unit = 3, iostat = rc)
+  IF (rc/=0) WRITE (*,*) 'Cannot open file ' , filename
 
-m = particles(:)%m
-print *, m
+  
+  READ (3, *) dt
+  READ (3, *) t_end
+  READ (3, *) dt_out
+  READ (3, *) n
 
+  ALLOCATE(particles(n))
+
+  DO i = 1, n
+     READ (3, *) particles(i)%m, particles(i)%p%x, particles(i)%p%y, particles(i)%p%z,&
+          &particles(i)%v%x, particles(i)%v%y, particles(i)%v%z
+  END DO   
+  CLOSE(3)
+
+  ALLOCATE(a(n))
+
+  a = vector3d(0.0,0.0,0.0)
+  CALL accelerations(particles)
+  
+  t_out = 0.0
+  t = 0.0
+  
+  OPEN (file = outname, action = 'write', status = 'replace', unit = 4, iostat = rc) !?
+    IF (rc/=0) WRITE (*,*) 'Cannot open file ' , outname
+  
+  WRITE(4, *) particles%p
+  DO WHILE (t <= t_end) 
+    t = t + dt
+    
+    particles%v = particles%v + a * dt/2. 
+    particles%p = particles%p + particles%v * dt 
+    
+    a = vector3d(0.0,0.0,0.0) 
+    CALL accelerations(particles) 
+    
+    particles%v = particles%v + a * dt/2.
+  
+    t_out = t_out + dt
+    
+    IF (t_out >= dt_out) THEN
+      WRITE(4, *) particles%p
+      t_out = 0.0
+    END IF
+    
+  END DO
+
+  CLOSE(4) 
+
+  
+  CONTAINS
+  
+  SUBROUTINE accelerations(particles)
+    TYPE(particle3d), DIMENSION(:), INTENT(in) :: particles ! idk if this is necessary neither :-(
+    INTEGER :: i,j ! idk if this is necessary	
+    DO i = 1,n
+      DO j = i+1,n
+        rji = particles(j)%p - particles(i)%p
+    	r =  distance(particles(j)%p, particles(i)%p)
+    	a(i) = a(i) + particles(j)%m * rji / (r+1.5)**3
+    	a(j) = a(j) - particles(i)%m * rji / (r+1.5)**3
+      END DO
+    END DO
+
+  END SUBROUTINE accelerations
+  
 end program e1 
