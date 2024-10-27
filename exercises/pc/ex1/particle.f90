04338a0c130f20e5d03ed0c0247d578e5870442a
Cathaysa <cathaysaperdomo@outlook.es>
2024-10-27 23:54:13 +0000

    Added comments to modules and program

--- exercises/pc/ex1/particle.f90
+++ exercises/pc/ex1/particle.f90
@@ -1,12 +1,13 @@
 module particle
   use geometry
   implicit none
-  
+
+  !Define a type for a 3D particle with position, velocity, acceleration and mass
   type :: particle3d
-     type(point3d) :: p !particle's position
-     type(vector3d) :: v !particle's velocity
-     type(vector3d) :: a !particle's acceleration
-     real(real64) :: m !particles's mass
+     type(point3d) :: p  !Position of particle
+     type(vector3d) :: v !Velocity of particle
+     type(vector3d) :: a !Acceleration of particle
+     real(real64) :: m   !Mass of particle
   end type particle3d
-  
+
 end module particle
