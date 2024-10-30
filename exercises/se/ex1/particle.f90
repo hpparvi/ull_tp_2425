de41b7a67e2b0c92339b0efaa6993f31a874281b
Eva Sola <evasolavi@gmail.es>
2024-10-30 15:44:38 +0000

    Change in real 64-bit declaration
    Changed makefile to exclude the declaration for the
    64-bit precision and instead added it in all the .f90
    files using the module iso_fortran_env and declaring
    each real variable as REAL(real64).

--- exercises/se/ex1/particle.f90
+++ exercises/se/ex1/particle.f90
@@ -1,11 +1,12 @@
 MODULE particle
+  USE, INTRINSIC :: iso_fortran_env
   USE geometry
   IMPLICIT NONE
 
   TYPE particle3d
      TYPE(point3d)  :: p ! Position
      TYPE(vector3d) :: v ! Velocity
-     REAL :: m ! Mass
+     REAL(real64) :: m ! Mass
   END TYPE particle3d
 
 
