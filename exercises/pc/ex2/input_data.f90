b20669f7c59b5af626171da164134708ce5509a8
Cathaysa <cathaysaperdomo@outlook.es>
2024-11-11 22:07:08 +0000

    Changed program to calculate initial data

--- exercises/pc/ex2/input_data.f90
+++ exercises/pc/ex2/input_data.f90
@@ -1,11 +1,13 @@
 PROGRAM input_data
+  USE particle !Importing the particle module 
   USE iso_fortran_env !Importing iso_fortran_env to specify the number of bits of the variables
   IMPLICIT NONE
   
   INTEGER(INT64) :: i, n
   INTEGER :: values(1:8), k
   INTEGER, dimension(:), allocatable :: seed
-  REAL(REAL64) :: mass, rx, ry, rz
+  TYPE(particle3d), allocatable :: p(:) !Particles
+  REAL(REAL64) :: rx, ry, rz
 
   CHARACTER(len = 50) :: input  !Input file name for initial data
   INTEGER :: io_status !Variable to check the status of I/O operations
@@ -22,8 +24,10 @@ PROGRAM input_data
   
   PRINT*, "Insert the number of bodies: "
   READ*, n
+
+  ALLOCATE(p(n))
   
-  mass = 1.0/n
+  p%m = 1.0/n
 
   !Open the input file and check for errors
   OPEN(12, file = input, status = 'replace', action = 'write', iostat = io_status)
@@ -37,18 +41,21 @@ PROGRAM input_data
   
   DO i = 1, n
      CALL random_number(rx)
+     p(i)%p%x = rx
      
      DO
         CALL random_number(ry)
+        p(i)%p%y = ry
         IF ((rx**2 + ry**2) .LE. 1) EXIT
      END DO
      
      DO
         CALL random_number(rz)
+        p(i)%p%z = rz
         IF ((rx**2 + ry**2 + rz**2) .LE. 1) EXIT
      END DO
      
-     WRITE(12, "(I3, 7X, 7ES12.4)") i, mass, rx, ry, rz, 0.0, 0.0, 0.0 !Write the mass and the current position and velocity of each particle to the input file
+     WRITE(12, "(I3, 7X, 7(ES12.4))") i, p(i)%m, rx, ry, rz, 0.0, 0.0, 0.0 !Write the mass and the current position and velocity of each particle to the input file
   END DO
 
   CLOSE(12)!Close the input file
