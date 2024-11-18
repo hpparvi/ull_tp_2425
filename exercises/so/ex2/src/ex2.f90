827580e890ed6d3eb67c64d831b308d99c97d81d
Oscar Soler <alu0101327365@ull.edu.es>
2024-11-18 00:50:11 +0000

    Setup finished (makefile, quick execution, general working, data reading and so). Barnes-hut not applied yet...

--- exercises/so/ex2/src/ex2.f90
+++ exercises/so/ex2/src/ex2.f90
@@ -3,101 +3,106 @@ PROGRAM leapfrog
 
   use geometry
   use particles
+  use init_conds_mod
+  use parameter_reader
 
   IMPLICIT NONE
 
   INTEGER :: i,j,k, i_t, ios
-  INTEGER :: n, n_t
+  INTEGER :: n, n_t!, N_bodies
   INTEGER :: num_args       ! to count the number of given arguments
-  character(len=100) :: filename     ! to import the stars data
-  character(len=256) :: dummy_line ! to ignore first line of file when reading data
-  character(len=100)  :: output_file = "output.txt"
-  real :: dt, t_end, t, dt_out, t_out
-  real :: rs, r2, r3
+  character(len=100) :: par_filename, dummy_line     ! to import the stars data and parameters data
+  !character(len=100)  :: !input_file, output_file
+  real :: t, t_out !dt, t_end, dt_out
+  !logical :: create_bodies
+  character(len=100) :: fmt
   
   type(particle), dimension(:), allocatable :: partics
   type(vector3d)                            :: rji_v
   real                                      :: r3_2
-  
-  real, DIMENSION(:), ALLOCATABLE :: m
-  real, DIMENSION(:,:), ALLOCATABLE :: r,v,a
-  real, DIMENSION(3) :: rji
 
+
+  !Expected usage:
+  ! - command line: executable <ic_data_filename> <time_param_filename>
+  
+  !call create_init_conds()
   ! Get the number of command-line arguments
     num_args = command_argument_count()
 
     if (num_args < 1) then
-        print *, "Error: No file name provided. Usage: ./program <filename>"
+        print *, "Error: No parameters file provided. Usage: ./program <par_file.par>"
         stop
     end if
 
     ! Get the input file name from the command-line arguments
-    call get_command_argument(1, filename)
+    call get_command_argument(1, par_filename)
 
     ! Remove any trailing spaces from the filename
-    filename = trim(adjustl(filename))
+    par_filename = trim(adjustl(par_filename))
 
-    ! Open the file
-    open(unit=10, file=filename, status="old", action="read", iostat=ios)
-    if (ios /= 0) then
-        print *, "Error: Could not open file ", filename
-        stop
+    call read_parameters(par_filename)
+    print*, "Reading parameters from ", par_filename
+    print*, "..."
+    print*, "Parameters loaded:"
+    print*, "  -  dt = ", dt
+    print*, "  -  dt_out = ", dt_out
+    print*, "  -  t_end = ", t_end
+    print*, "  -  input_file = ", input_file
+    print*, "  -  create_bodies = ", create_bodies
+    if (create_bodies .eqv. .true.) then
+    print*, "  -  N_bodies = ", N_bodies
+ end if
+    print*, "  -  output_file = ", output_file
+
+    input_file = trim(input_file)
+ 
+
+    if (create_bodies .eqv. .true.) then
+       call create_init_conds(input_file, N_bodies)
     end if
-
+    
+    ! Open the file with initial data
+    open(unit=10, file=input_file, status="old", action="read", form="formatted", iostat=ios)
+    if (ios /= 0) then
+        print *, "Error: Could not open file ", input_file
+        stop
+     end if
+     
     ! Count the number of stars
     n = 0
     do
-        read(10, '(A)', iostat=ios) ! reading each line as a whole string
-        if (ios /= 0) exit
+       read(10, '(A)', iostat=ios) dummy_line  ! reading each line as a whole string
+       if (ios /= 0) then
+          if (ios == 1) then
+          print*, "Error reading ", trim(input_file)
+          end if
+          if (ios == -1) then
+             exit
+          end if
+          
+       end if
+       
         n = n + 1
      end do
-     n = n-1 ! to remove the first line with time parameters
     rewind(10) !so that pointer is at the beginnin for reading and importing data later
 
+    print*, " Number of bodies = ", n
+
     ! Allocate arrays based on the number of stars
-! Particle method for calculations
- allocate(partics(n))
-! No particle method (original one) used for checking. Results must match
- ALLOCATE(m(n))
- ALLOCATE(r(n,3))
- ALLOCATE(v(n,3))
- ALLOCATE(a(n,3))
-    
- ! Read the data into arrays (no particle method)
- read(10, '(A)', iostat=ios) dummy_line
- if (ios /= 0) then
-        print *, "Error: Could not read the first line"
-        stop
-     end if
-     
-    do i = 1, n
-        read(10, *, iostat=ios) m(i), r(i,1), r(i,2), r(i,3), v(i,1), v(i,2), v(i,3)
-        if (ios /= 0) then
-            print *, "Error reading file, stopped at star:", i
-            exit
-        end if
-     end do
-     print *, "Values saved to stars (no particle method)"
-rewind(10)
-     
-! Repeat reading for particle variables
-read(10, *, iostat = ios) dt, dt_out, t_end
- if (ios /= 0) then
-        print *, "Error: Could not read the first line"
-        stop
-     end if
+    allocate(partics(n))
      
     do i = 1, n
         read(10, *, iostat=ios) partics(i)%m, partics(i)%p%x, partics(i)%p%y, partics(i)%p%z, partics(i)%v
         if (ios /= 0) then
             print *, "Error reading file, stopped at star:", i
             exit
-        end if
+         end if
      end do
-     print *, "Values saved to particle type variables"
+     print *, "Values saved to particle type variables."
 
     ! Close the file
-    close(10)
+     close(10)
+     
 
   n_t = t_end/dt
 
@@ -110,15 +115,9 @@ read(10, *, iostat = ios) dt, dt_out, t_end
      DO j = i+1,n
         ! Particles method
         rji_v = vecpp(partics(i)%p, partics(j)%p)
-        r3_2 = norm(rji_v)**3
+        r3_2 = norm(rji_v)**3 + epsilon **2
         partics(i)%a = partics(i)%a + (partics(j)%m * rji_v / r3_2)
         partics(j)%a = partics(j)%a - (partics(i)%m * rji_v / r3_2)
-        ! Original method
-        rji = r(j,:) - r(i,:)
-        r2 = SUM(rji**2)
-        r3 = r2 * SQRT(r2)
-        a(i,:) = a(i,:) + m(j) * rji / r3
-        a(j,:) = a(j,:) - m(i) * rji / r3
      END DO
   END DO
   
@@ -127,52 +126,36 @@ read(10, *, iostat = ios) dt, dt_out, t_end
   ! Open the output file
   open(unit=20, file=output_file, status="replace", action="write", iostat=ios)
      if (ios /= 0) then
-        print *, "Error: Could not open file ", filename
+        print *, "Error: Could not open file ", output_file
         stop
      end if
      
 ! start all calculations
   DO i_t = 0, n_t
      t = i_t * dt
-     ! Particles method
      partics(:)%v = partics(:)%v + (partics(:)%a * (dt/2))
      partics(:)%p = partics(:)%p + (partics(:)%v * dt)
      partics(:)%a = vector3d(0,0,0)
-     ! Original method
-     v = v + a * dt/2
-     r = r + v * dt
-     a = 0.0
-     
+    
      DO i = 1,n
         DO j = i+1,n
-           ! Particles method
            rji_v = vecpp(partics(i)%p, partics(j)%p)
            r3_2 = norm(rji_v)**3
            partics(i)%a = partics(i)%a + (partics(j)%m * rji_v / r3_2)
            partics(j)%a = partics(j)%a - (partics(i)%m * rji_v / r3_2)
-           ! Original method
-           rji = r(j,:) - r(i,:)
-           r2 = SUM(rji**2)
-           r3 = r2 * SQRT(r2)
-           a(i,:) = a(i,:) + m(j) * rji / r3
-           a(j,:) = a(j,:) - m(i) * rji / r3
         END DO
-        ! Store all values of positions:
-     write(20, '(I5, F12.2, 3F20.15)') i_t, t, partics(i)%p
-     write(20, '(I5, F12.2, 3F20.15)') i_t, t, r(i,:)
      END DO
-     !Particles method
+     ! Store all values of positions:
+     ! t p1x, p1y, ... pNz
+     write(fmt, "(A,I0,A,I0,A)") "(F10.2, ", 3*N, "F12.5)"
+     write(20, fmt) t, (partics(k)%p, k=1,N)
      partics(:)%v = partics(:)%v + (partics(:)%a * (dt/2))
-     ! Original method
-     v = v + a * dt/2
-     
      t_out = t_out + dt
      IF (t_out >= dt_out) THEN
         print*, "Time = ", t, "", "Iteration = ", i_t
         DO i = 1,n
-           print*, "Particle ", i
-           print*, partics(i)%p
-           print*, r(i,:)
+           !print*, "Particle ", i
+           !print*, partics(i)%p
         END DO
         t_out = 0.0
      END IF
--- exercises/so/ex2/src/init_conds.f90
+++ exercises/so/ex2/src/init_conds.f90
@@ -1,21 +1,66 @@
-PROGRAM init_cond
+MODULE init_conds_mod
+  IMPLICIT NONE
+CONTAINS
+  SUBROUTINE create_init_conds(output_name, N)
+
   IMPLICIT NONE
   
-  INTEGER :: I,N
+  integer, intent(in) :: N
+  CHARACTER(len=100) , intent(in):: output_name  !  = "data/random_bodies.txt"
+
+  INTEGER :: I
   INTEGER :: values(1:8), k
   INTEGER, dimension(:), allocatable :: seed
   REAL :: mass,rx,ry,rz
   
+  !to count the number of arguments. If there is one, use as the number of  particles.
+  !If there is  none, ask for the number of particles to be created
+  INTEGER:: num_args, ios
+  CHARACTER(len=100) :: n_string
+
+  CHARACTER(len=100) :: dummy_string
+
+
   CALL date_and_time(values=values)
   CALL random_seed(size=k)
   
   ALLOCATE(seed(1:k))
   SEED(:) = values(8)
   CALL random_seed(put=seed)
+
+  print *, "Creating random initial conditions with ", N, "bodies."
+
+ ! num_args = command_argument_count()
+ ! if (num_args<1) then
+ !    print *, "No number of bodies provided to generate initial conditions."
+ !    print *, "You can skip this step by stating the number of bodies when  exectuing:"
+ !    print *, "  ./program <number_bodies>"
+ !    PRINT*, "You can also change the output file name by executing ./program <number_bodies> <output_name.txt>"
+ !    PRINT *,""
+ !    PRINT*, "Type the number of bodies desired."
+ !    READ*, N
+ ! else
+ !    call get_command_argument(1, n_string)
+ !    read(n_string, *, iostat=ios) N
+
+ !    PRINT*, N, " bodies distributed randomly through a sphere."
+ ! end if
+
+  !if (num_args>1) then
+ !    CALL get_command_argument(2, dummy_string)
+ !!    print *, "Changing output file name from ", output_name, " to ", dummy_string
+ !    CALL get_command_argument(2, output_name)
+ ! end if
   
-  PRINT*, "Number of bodies?"
-  READ*, N
   mass = 1.0 / N
+
+  ! Open the output file
+  open(unit=20, file=output_name, status="replace", action="write", iostat=ios)
+     if (ios /= 0) then
+        print *, "Error: Could not open file ", output_name
+        stop
+     end if
+     
   DO I= 1,N
      DO
         CALL random_number(rx)
@@ -28,5 +73,9 @@ PROGRAM init_cond
      END DO
      ! Now we have created a point with mass, r coordinates (inside sphere of radius 1)
      ! and we set initial velocities as 0
-     WRITE(*,’(F6.3, 3F11.8,3I2)’) mass, rx,ry,rz, 0,0,0
+     WRITE(20,'(F6.3, 3F20.15, 3I2)') mass, rx,ry,rz, 0,0,0
   END DO
+  close(20)
+  PRINT*, "Initial conditions created."
+END SUBROUTINE CREATE_INIT_CONDs
+END MODULE init_conds_mod
